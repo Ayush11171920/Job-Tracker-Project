f8db3e7aff3144d2673b0a215ef3ee100c2317d4
Ayush <gandhiayush150@gmail.com>
2026-02-13 15:37:36 +0530

    Configured production environment variables

--- job_tracker/settings.py
+++ job_tracker/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#f1i374b+4k7w(7dt3tfr&@54xd2r(11gdr!%&(pnu%f50qq9b'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = ['*']
 
